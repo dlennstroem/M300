1127b72c7625aee88ab21971f6595fe2f499cac3
simon.stumpf@hispeed.ch <simon.stumpf@hispeed.ch>
2019-05-05 13:26:25 +0200

    improvements docker compose file

--- LB3/php/index.php
+++ LB3/php/index.php
@@ -2,22 +2,7 @@
 <html>
 <body>
 
-<?php
-$servername = "localhost";
-$username = "dbuser";
-$password = "1234";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
-
-
+ 
 <p>The webserver too!! (duh)</p>
 
 </body>
